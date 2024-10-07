b55bac9a9fd48c7a2f89245a01a00ed7ccb70fe5
mazzzeen <mazen.nabih@gmail.com>
2024-10-07 08:31:06 +0300

    305 current  2024-10-07 08:31:04  24.11.20241004.bc947f5  6.6.53                          *

--- modules/home/nixvim.nix
+++ modules/home/nixvim.nix
@@ -21,63 +21,75 @@
         	end,
         })'';
       colorschemes.nord.enable = true;
-      plugins.lsp = {
-        enable = true;
-        servers = {
-          nil-ls = {
-            enable = true;
-            autostart = true;
-            settings = {
-              flake = {
-                autoArchive = true;
+      plugins = {
+        lsp = {
+          enable = true;
+          servers = {
+            nil-ls = {
+              enable = true;
+              autostart = true;
+              settings = {
+                flake = {
+                  autoArchive = true;
+                };
               };
             };
-          };
-          pylsp.enable = true;
-          lua-ls.enable = true;
-        };
-      };
-      plugins.cmp = {
-        enable = true;
-        autoEnableSources = true;
-        settings.sources = [
-          {name = "nvim_lsp";}
-          {name = "path";}
-          {name = "buffer";}
-        ];
-        settings = {
-          mapping = {
-            __raw = ''
-                  cmp.mapping.preset.insert({
-              ["<CR>"] = cmp.mapping.confirm({ select = true }),
-              ["<Tab>"] = cmp.mapping(function(fallback)
-                   	  if cmp.visible() then
-                         cmp.select_next_item()
-                       else
-                         fallback()
-                       end
-               	end, { "i", "s"})})
-            '';
+            pylsp.enable = true;
+            lua-ls.enable = true;
           };
         };
-      };
-      plugins.lualine = {
-        enable = true;
-        settings = {
-          options = {
-            component_separators = "|";
-            section_separators = {
-              left = "";
-              right = "";
+        cmp = {
+          enable = true;
+          autoEnableSources = true;
+          settings.sources = [
+            {name = "nvim_lsp";}
+            {name = "path";}
+            {name = "buffer";}
+          ];
+          settings = {
+            mapping = {
+              __raw = ''
+                    cmp.mapping.preset.insert({
+                ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                ["<Tab>"] = cmp.mapping(function(fallback)
+                     	  if cmp.visible() then
+                           cmp.select_next_item()
+                         else
+                           fallback()
+                         end
+                 	end, { "i", "s"})})
+              '';
             };
           };
-          sections = {
-            lualine_a = [{__raw = ''{ 'mode', separator = { left = '', right = '' } }'';}];
-            lualine_b = ["filename" "branch"];
-            lualine_c = ["%="];
-            lualine_x = [""];
-            lualine_y = ["filetype" "progress"];
-            lualine_z = [{__raw = ''{ 'location', separator = { left = '', right = '█' }, }'';}];
+        };
+        indent-blankline = {
+          enable = true;
+        };
+        treesitter = {
+          enable = true;
+          folding = true;
+          settings = {
+            highlight.enable = true;
+          };
+        };
+        lualine = {
+          enable = true;
+          settings = {
+            options = {
+              component_separators = "|";
+              section_separators = {
+                left = "";
+                right = "";
+              };
+            };
+            sections = {
+              lualine_a = [{__raw = ''{ 'mode', separator = { left = '', right = '' } }'';}];
+              lualine_b = ["filename" "branch"];
+              lualine_c = ["%="];
+              lualine_x = [""];
+              lualine_y = ["filetype" "progress"];
+              lualine_z = [{__raw = ''{ 'location', separator = { left = '', right = '█' }, }'';}];
+            };
           };
         };
       };
