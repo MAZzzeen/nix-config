4033145ce83f8899ebf0b699101457787ea6ba7c
mazzzeen <mazen.nabih@gmail.com>
2024-10-08 20:29:07 +0300

    397 current  2024-10-08 08:54:00  24.11.20241004.bc947f5  6.6.53                          *

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -79,15 +79,18 @@
     shell = pkgs.zsh;
   };
 
-  fonts.packages = with pkgs; [
-    noto-fonts
-    noto-fonts-cjk
-    noto-fonts-emoji
-    liberation_ttf
-    fira-code
-    fira-code-symbols
-    (nerdfonts.override {fonts = ["FiraCode" "DroidSansMono" "Inconsolata"];})
-  ];
+  fonts = {
+    packages = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+      liberation_ttf
+      fira-code
+      fira-code-symbols
+      (nerdfonts.override {fonts = ["FiraCode" "DroidSansMono" "Inconsolata"];})
+    ];
+    fontconfig.defaultFonts = {emoji = ["Noto Color Emoji"];};
+  };
 
   environment.systemPackages = with pkgs; [
     git
