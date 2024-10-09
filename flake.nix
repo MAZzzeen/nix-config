c212ebab78c91d76a729361b69f5e3a0c02a130f
mazzzeen <mazen.nabih@gmail.com>
2024-10-09 06:11:26 +0300

    415 current  2024-10-09 06:10:48  24.11.20241004.bc947f5  6.6.53                          *

--- flake.nix
+++ flake.nix
@@ -14,11 +14,6 @@
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    firefox-addons = {
-      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = {
