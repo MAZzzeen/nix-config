b45d81c38f1014b175fb70d1d363dd664ac003cb
mazzzeen <mazen.nabih@gmail.com>
2024-10-11 01:48:05 +0300

    447 current  2024-10-11 01:48:02  24.11.20241009.5633bcf  6.6.54                          *

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -11,72 +11,76 @@
     wayland.windowManager.hyprland = {
       enable = true;
       extraConfig = ''
-        bind = SUPER, Return, exec, alacritty
-        bind = SUPER, C, killactive,
-        bind = SUPER, M, exit,
-        bind = SUPER, E, exec, thunar
-        bind = SUPER, V, togglefloating,
-        bind = SUPER, Space, exec, rofi -show drun -show-icons
-        bind = SUPER, P, pseudo,
-        bind = SUPER, J, togglesplit,
-        bind = SUPER, Escape, exec, wlogout
-        bind = SUPER, F, fullscreen,
-
-        bind =, Print, exec, slurp | grim -g - - | wl-copy
-        bind = Shift, Print, exec, grim - | wl-copy
-
-        bind = SUPER, left, movefocus, l
-        bind = SUPER, right, movefocus, r
-        bind = SUPER, up, movefocus, u
-        bind = SUPER, down, movefocus, d
-
-        bind = SUPER, 1, workspace, 1
-        bind = SUPER, 2, workspace, 2
-        bind = SUPER, 3, workspace, 3
-        bind = SUPER, 4, workspace, 4
-        bind = SUPER, 5, workspace, 5
-        bind = SUPER, 6, workspace, 6
-        bind = SUPER, 7, workspace, 7
-        bind = SUPER, 8, workspace, 8
-        bind = SUPER, 9, workspace, 9
-        bind = SUPER, 0, workspace, 10
-
-        bind = SUPER SHIFT, 1, movetoworkspace, 1
-        bind = SUPER SHIFT, 2, movetoworkspace, 2
-        bind = SUPER SHIFT, 3, movetoworkspace, 3
-        bind = SUPER SHIFT, 4, movetoworkspace, 4
-        bind = SUPER SHIFT, 5, movetoworkspace, 5
-        bind = SUPER SHIFT, 6, movetoworkspace, 6
-        bind = SUPER SHIFT, 7, movetoworkspace, 7
-        bind = SUPER SHIFT, 8, movetoworkspace, 8
-        bind = SUPER SHIFT, 9, movetoworkspace, 9
-        bind = SUPER SHIFT, 0, movetoworkspace, 10
-
-        bind = SUPER, S, togglespecialworkspace, magic
-        bind = SUPER SHIFT, S, movetoworkspace, special:magic
-
-        bind = SUPER, mouse_down, workspace, e+1
-        bind = SUPER, mouse_up, workspace, e-1
-
-        bindel = ,XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+
-        bindel = ,XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-
-        bindel = ,XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
-        bindel = ,XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle
-        bindel = ,XF86MonBrightnessUp, exec, brightnessctl s 10%+
-        bindel = ,XF86MonBrightnessDown, exec, brightnessctl s 10%-
-
-        bindl = , XF86AudioNext, exec, playerctl next
-        bindl = , XF86AudioPause, exec, playerctl play-pause
-        bindl = , XF86AudioPlay, exec, playerctl play-pause
-        bindl = , XF86AudioPrev, exec, playerctl previous
         windowrule = float, ^(nekobox)$
         windowrulev2 = suppressevent maximize, class:.*
       '';
       importantPrefixes = ["$" "bezier" "name" "source"];
       plugins = [];
       settings = {
-        monitor = [",preferred,auto,1" "FALLBACK,1920x1080@60,auto,1"];
+        monitor = [",preferred,auto,auto" "DP-1,preferred,auto,1.6" "FALLBACK,1920x1080@60,auto,1"];
         bindm = ["SUPER, mouse:272, movewindow" "SUPER, mouse:273, resizewindow"];
+        bindl = [
+          ", XF86AudioNext, exec, playerctl next"
+          ", XF86AudioPause, exec, playerctl play-pause"
+          ", XF86AudioPlay, exec, playerctl play-pause"
+          ", XF86AudioPrev, exec, playerctl previous"
+        ];
+        bindel = [
+          ",XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"
+          ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+          ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+          ",XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+          ",XF86MonBrightnessUp, exec, brightnessctl s 10%+"
+          ",XF86MonBrightnessDown, exec, brightnessctl s 10%-"
+        ];
+        bind = [
+          "SUPER, Return, exec, alacritty"
+          "SUPER, C, killactive,"
+          "SUPER, M, exit,"
+          "SUPER, E, exec, thunar"
+          "SUPER, V, togglefloating,"
+          "SUPER, Space, exec, rofi -show drun -show-icons"
+          "SUPER, P, pseudo,"
+          "SUPER, J, togglesplit,"
+          "SUPER, Escape, exec, wlogout"
+          "SUPER, F, fullscreen,"
+
+          ", Print, exec, slurp | grim -g - - | wl-copy"
+          "Shift, Print, exec, grim - | wl-copy"
+
+          "SUPER, left, movefocus, l"
+          "SUPER, right, movefocus, r"
+          "SUPER, up, movefocus, u"
+          "SUPER, down, movefocus, d"
+
+          "SUPER, 1, workspace, 1"
+          "SUPER, 2, workspace, 2"
+          "SUPER, 3, workspace, 3"
+          "SUPER, 4, workspace, 4"
+          "SUPER, 5, workspace, 5"
+          "SUPER, 6, workspace, 6"
+          "SUPER, 7, workspace, 7"
+          "SUPER, 8, workspace, 8"
+          "SUPER, 9, workspace, 9"
+          "SUPER, 0, workspace, 10"
+
+          "SUPER SHIFT, 1, movetoworkspace, 1"
+          "SUPER SHIFT, 2, movetoworkspace, 2"
+          "SUPER SHIFT, 3, movetoworkspace, 3"
+          "SUPER SHIFT, 4, movetoworkspace, 4"
+          "SUPER SHIFT, 5, movetoworkspace, 5"
+          "SUPER SHIFT, 6, movetoworkspace, 6"
+          "SUPER SHIFT, 7, movetoworkspace, 7"
+          "SUPER SHIFT, 8, movetoworkspace, 8"
+          "SUPER SHIFT, 9, movetoworkspace, 9"
+          "SUPER SHIFT, 0, movetoworkspace, 10"
+
+          "SUPER, S, togglespecialworkspace, magic"
+          "SUPER SHIFT, S, movetoworkspace, special:magic"
+
+          "SUPER, mouse_down, workspace, e+1"
+          "SUPER, mouse_up, workspace, e-1"
+        ];
         exec-once = "hyprpaper & lxsession & hypridle & mako & wl-paste --watch cliphist store & waybar";
         general = {
           gaps_in = 8;
