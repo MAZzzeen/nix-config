0e51b160c9e9130ae198010ce00264a9d9f55ec2
mazzzeen <mazen.nabih@gmail.com>
2024-10-11 17:15:32 +0300

    481 current  2024-10-11 17:15:29  24.11.20241009.5633bcf  6.6.54                          *

--- modules/home/yazi.nix
+++ modules/home/yazi.nix
@@ -19,76 +19,76 @@
         opener = ''
           edit = [
             {
-              run = "nvim '$@'";
-              block = true;
+              run = "nvim '$@'",
+              block = true
             }
-          ];
+          ]
           play = [
             {
-              run = "mpv '$@'";
-              orphan = true;
+              run = "mpv '$@'",
+              orphan = true
             }
-          ];
+          ]
           read = [
             {
-              run = "zathura '$@'";
-              orphan = true;
+              run = "zathura '$@'",
+              orphan = true
             }
-          ];
+          ]
           view = [
             {
-              run = "nsxiv '$@'";
-              orphan = true;
+              run = "nsxiv '$@'",
+              orphan = true
             }
-          ];
+          ]
           open = [
             {
-              run = "xdg-open '$@'";
-              desc = "Open";
+              run = "xdg-open '$@'",
+              desc = "Open"
             }
-          ];
+          ]
         '';
         preview = {
           wrap = "yes";
         };
-        open = {
+        open = ''
           append_rules = [
             {
-              name = "*";
-              use = "open";
+              name = "*",
+              use = "open"
             }
-          ];
+          ],
           rules = [
             {
-              mime = "text/*";
-              use = "edit";
-            }
+              mime = "text/*",
+              use = "edit"
+            },
             {
-              mime = "video/*";
-              use = "play";
-            }
+              mime = "video/*",
+              use = "play"
+            },
             {
-              mime = "audio/*";
-              use = "play";
-            }
+              mime = "audio/*",
+              use = "play"
+            },
             {
-              mime = "applications/pdf";
-              use = "read";
-            }
+              mime = "applications/pdf",
+              use = "read"
+            },
             {
-              name = "*.cbr";
-              use = "read";
-            }
+              name = "*.cbr",
+              use = "read"
+            },
             {
-              name = "*.epub";
-              use = "read";
-            }
+              name = "*.epub",
+              use = "read"
+            },
             {
-              name = "*.djvu";
-              use = "read";
+              name = "*.djvu",
+              use = "read"
             }
-          ];
-        };
+          ]
+        '';
       };
     };
   };
