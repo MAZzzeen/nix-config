4d04894887579e24890ffb0bd69eab9a1b961b59
mazzzeen <mazen.nabih@gmail.com>
2024-10-13 03:31:59 +0300

    519 current  2024-10-13 03:31:56  24.11.20241009.5633bcf  6.6.54                          *

--- modules/sys/stylix.nix
+++ modules/sys/stylix.nix
@@ -10,6 +10,7 @@
   config = lib.mkIf config.sstylix.enable {
     stylix = {
       enable = true;
+      polarity = "dark";
       image = ./../../Backgrounds/Wallpaper.jpg;
       # base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
       autoEnable = true;
