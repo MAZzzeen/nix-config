988194ad6f7ce8b6752766f8342c60d2e2bd56a7
mazzzeen <mazen.nabih@gmail.com>
2024-10-14 18:13:33 +0300

    618 current  2024-10-14 18:13:29  24.11.20241009.5633bcf  6.6.54                          *

--- modules/home/rofi.nix
+++ modules/home/rofi.nix
@@ -14,9 +14,9 @@
       package = pkgs.rofi-wayland;
       cycle = true;
       extraConfig = {
-        display-drun = " ";
-        display-emoji = "󰞅 ";
-        display-run = " ";
+        display-drun = "";
+        display-emoji = "󰞅";
+        display-run = "";
         modi = "drun,run,emoji";
         drun-display-format = "{name}";
         window-format = "{w} · {c} · {t}";
@@ -24,125 +24,115 @@
       };
       theme = let
         inherit (config.lib.formats.rasi) mkLiteral;
-      in {
-        window = with config.lib.stylix.colors.withHashtag; {
-          transparency = "real";
-          width = mkLiteral "600px";
-          enabled = true;
-          border = mkLiteral "2px solid";
-          border-radius = mkLiteral "15px";
-          border-color = mkLiteral "${base0C}";
-          cursor = mkLiteral "default";
+      in
+        with config.lib.stylix.colors.withHashtag; {
+          window = {
+            transparency = "real";
+            width = mkLiteral "600px";
+            enabled = true;
+            border = mkLiteral "2px solid";
+            border-radius = mkLiteral "15px";
+            border-color = mkLiteral "${base0C}";
+            cursor = mkLiteral "default";
+          };
+          mainbox = {
+            enabled = true;
+            spacing = mkLiteral "10px";
+            margin = mkLiteral "0px";
+            padding = mkLiteral "20px";
+            border = mkLiteral "0px solid";
+            background-color = mkLiteral "transparent";
+            children = mkLiteral ''[ "inputbar", "message", "listview" ]'';
+          };
+          inputbar = {
+            enabled = true;
+            spacing = mkLiteral "10px";
+            background-color = mkLiteral "transparent";
+            children = mkLiteral ''[ "textbox-prompt-colon", "entry", "mode-switcher" ]'';
+          };
+          prompt = {
+            enabled = true;
+          };
+          num-filtered-rows = {
+            enabled = true;
+            expand = false;
+          };
+          textbox-num-sep = {
+            enabled = true;
+            expand = false;
+            str = "/";
+          };
+          num-rows = {
+            enabled = true;
+            expand = false;
+          };
+          case-indicator = {
+            enabled = true;
+          };
+          listview = {
+            enabled = true;
+            columns = 1;
+            lines = 8;
+            cycle = true;
+            dynamic = true;
+            scrollbar = true;
+            layout = mkLiteral "vertical";
+            reverse = false;
+            fixed-height = true;
+            fixed-columns = true;
+            spacing = mkLiteral "5px";
+            background-color = mkLiteral "transparent";
+            cursor = "default";
+          };
+          scrollbar = {
+            handle-width = mkLiteral "5px";
+            border-radius = mkLiteral "10px";
+          };
+          element = {
+            enabled = true;
+            spacing = mkLiteral "10px";
+            margin = mkLiteral "0px";
+            padding = mkLiteral "5px 10px";
+            border-radius = mkLiteral "10px";
+            background-color = mkLiteral "transparent";
+            cursor = mkLiteral "pointer";
+          };
+          element-icon = {
+            size = mkLiteral "24px";
+          };
+          element-text = {
+            vertical-align = mkLiteral "0.5";
+            horizontal-align = 0;
+          };
+          mode-switcher = {
+            enabled = true;
+            spacing = mkLiteral "10px";
+            background-color = mkLiteral "transparent";
+          };
+          button = {
+            padding = mkLiteral "5px 10px";
+            border-radius = mkLiteral "10px";
+            cursor = mkLiteral "pointer";
+          };
+          message = {
+            enabled = true;
+            background-color = mkLiteral "transparent";
+          };
+          textbox = {
+            padding = mkLiteral "8px 10px";
+            border-radius = mkLiteral "10px";
+            vertical-align = mkLiteral "0.5";
+            horizontal-align = 0;
+            highlight = mkLiteral "none";
+            blink = true;
+            markup = true;
+          };
+          error-message = {
+            padding = mkLiteral "10px";
+            border = mkLiteral "2px solid";
+            border-radius = mkLiteral "10px";
+          };
         };
-        mainbox = {
-          enabled = true;
-          spacing = mkLiteral "10px";
-          margin = mkLiteral "0px";
-          padding = mkLiteral "20px";
-          border = mkLiteral "0px solid";
-          border-radius = mkLiteral "0px 0px 0px 0px";
-          background-color = mkLiteral "transparent";
-          children = mkLiteral ''[ "inputbar", "message", "listview" ]'';
-        };
-        inputbar = {
-          enabled = true;
-          spacing = mkLiteral "10px";
-          margin = mkLiteral "0px";
-          padding = mkLiteral "0px";
-          border = mkLiteral "0px solid";
-          border-radius = mkLiteral "0px";
-          background-color = mkLiteral "transparent";
-          children = mkLiteral ''[ "textbox-prompt-colon", "entry", "mode-switcher" ]'';
-        };
-        prompt = {
-          enabled = true;
-        };
-        num-filtered-rows = {
-          enabled = true;
-          expand = false;
-        };
-        textbox-num-sep = {
-          enabled = true;
-          expand = false;
-          str = "/";
-        };
-        num-rows = {
-          enabled = true;
-          expand = false;
-        };
-        case-indicator = {
-          enabled = true;
-        };
-        listview = {
-          enabled = true;
-          columns = 1;
-          lines = 8;
-          cycle = true;
-          dynamic = true;
-          scrollbar = true;
-          layout = mkLiteral "vertical";
-          reverse = false;
-          fixed-height = true;
-          fixed-columns = true;
-
-          spacing = mkLiteral "5px";
-          margin = mkLiteral "0px";
-          padding = mkLiteral "0px";
-          border = mkLiteral "0px solid";
-          border-radius = mkLiteral "0px";
-          background-color = mkLiteral "transparent";
-          cursor = "default";
-        };
-        scrollbar = {
-          handle-width = mkLiteral "5px";
-          border-radius = mkLiteral "10px";
-        };
-        element = {
-          enabled = true;
-          spacing = mkLiteral "10px";
-          margin = mkLiteral "0px";
-          padding = mkLiteral "5px 10px";
-          border = mkLiteral "0px solid";
-          border-radius = mkLiteral "10px";
-          background-color = mkLiteral "transparent";
-          cursor = mkLiteral "pointer";
-        };
-        element-icon = {
-          size = mkLiteral "24px";
-        };
-        element-text = {
-          vertical-align = mkLiteral "0.5";
-          horizontal-align = 0;
-        };
-        mode-switcher = {
-          enabled = true;
-          spacing = mkLiteral "10px";
-          background-color = mkLiteral "transparent";
-        };
-        button = {
-          padding = mkLiteral "5px 10px";
-          border-radius = mkLiteral "10px";
-          cursor = mkLiteral "pointer";
-        };
-        message = {
-          enabled = true;
-          background-color = mkLiteral "transparent";
-        };
-        textbox = {
-          padding = mkLiteral "8px 10px";
-          border-radius = mkLiteral "10px";
-          vertical-align = mkLiteral "0.5";
-          horizontal-align = 0;
-          highlight = mkLiteral "none";
-          blink = true;
-          markup = true;
-        };
-        error-message = {
-          padding = mkLiteral "10px";
-          border = mkLiteral "2px solid";
-          border-radius = mkLiteral "10px";
-        };
-      };
     };
   };
 }
