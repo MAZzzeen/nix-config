d0b829312509cca48a71690a6e0a08e298f85df2
mazzzeen <mazen.nabih@gmail.com>
2024-10-14 07:04:13 +0300

    592 current  2024-10-14 07:04:10  24.11.20241009.5633bcf  6.6.54                          *

--- modules/home/rofi.nix
+++ modules/home/rofi.nix
@@ -23,9 +23,19 @@
       theme = let
         inherit (config.lib.formats.rasi) mkLiteral;
       in {
-        "window" = {
-          "border" = "20";
-          "width" = "100";
+        "*" = {
+          width = 512;
+        };
+
+        "#inputbar" = {
+          children = map mkLiteral ["prompt" "entry"];
+        };
+
+        "#textbox-prompt-colon" = {
+          expand = false;
+          str = ":";
+          margin = mkLiteral "0px 0.3em 0em 0em";
+          text-color = mkLiteral "@foreground-color";
         };
       };
     };
