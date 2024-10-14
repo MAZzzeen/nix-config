7267f34da584d6a8702089a08eb6b6be9375ff95
mazzzeen <mazen.nabih@gmail.com>
2024-10-14 18:35:11 +0300

    632 current  2024-10-14 18:35:07  24.11.20241009.5633bcf  6.6.54                          *

--- modules/home/rofi.nix
+++ modules/home/rofi.nix
@@ -126,7 +126,7 @@
             background-color = mkLiteral "transparent";
           };
           button = {
-            padding = mkLiteral "5px 12px 10px 5px";
+            padding = mkLiteral "10px 12px 5px 5px";
             border-radius = mkLiteral "10px";
             cursor = mkLiteral "pointer";
           };
